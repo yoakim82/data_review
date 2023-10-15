b1ab5e330aaaffd924a7ad51fa15d5c7b577f10c
stjoly <joakim.linden@saabgroup.com>
2023-10-16 00:40:15 +0200

    added script to trim out bboxes smaller than X pixels. If an object is trimmed, it will be moved to the removed_vehicles/removed_bboxes sections of the json file. If all bboxes are removed, the timmed_images.sh script is appended with this particular image, queueing it for deletion.

---
+++ trim_small_boxes.py
@@ -0,0 +1,98 @@
+import glob
+import json
+import os
+
+import matplotlib.pyplot as plt
+
+
+def append_to_script(script_file_name, command):
+    with open(script_file_name, "a") as script_file:
+        script_file.write(command + "\n")
+
+def process_json(json_file, area_threshold, box_areas, removed_box_areas, trim_script):
+    with open(json_file, 'r') as f:
+        data = json.load(f)
+
+    bboxes = data["bboxes"]
+    vehicle_class = data["vehicle_class"]
+    removed_bboxes = data["removed_bboxes"]
+    removed_vehicle_class = data["removed_vehicle_class"]
+
+    i, j = 0, 0
+    while j < len(removed_bboxes):
+        rbox = removed_bboxes[j]
+        min_x, min_y = rbox[0]
+        max_x, max_y = rbox[1]
+        area = (max_x - min_x) * (max_y - min_y)
+
+        #print(f"INFO: area of removed box {j} is {area} pixels.")
+        j += 1
+
+        removed_box_areas.append(area)
+
+
+    while i < len(bboxes):
+        box = bboxes[i]
+        min_x, min_y = box[0]
+        max_x, max_y = box[1]
+        area = (max_x - min_x) * (max_y - min_y)
+
+        if area < area_threshold:
+            print(f"area of box {i} is {area} pixels: too small, removing it.")
+            removed_bboxes.append(bboxes.pop(i))
+            removed_vehicle_class.append(vehicle_class.pop(i))
+        else:
+            print(f"box {i} with area {area} pixels is kept.")
+
+        i += 1
+
+        box_areas.append(area)
+
+    if len(bboxes) == 0:
+        file_name = os.path.basename(json_file)
+        append_to_script(script_file_name=trim_script, command=f"find . -name \x5c{file_name.split('.')[0]}.* -type f -delete")
+
+    if area_threshold > 0:
+        data["bboxes"] = bboxes
+        data["vehicle_class"] = vehicle_class
+        data["removed_bboxes"] = removed_bboxes
+        data["removed_vehicle_class"] = removed_vehicle_class
+
+        with open(json_file, 'w') as f:
+            json.dump(data, f, indent=4)
+
+
+def process_files(pattern, area_threshold):
+    file_list = glob.glob(pattern)
+    box_areas = []
+    removed_box_areas = []
+    trim_script = "trimmed_images.sh"
+
+    for file in file_list:
+        print(f"processing file {file}:")
+        process_json(file, area_threshold, box_areas, removed_box_areas, trim_script)
+
+    return box_areas, removed_box_areas
+
+# Example usage:
+#json_file = "json_test.txt"
+#area_threshold = 100  # You can set your own threshold here
+#process_json(json_file, area_threshold)
+
+
+# Example usage:
+pattern = "test/world_0_drones_1/out_bbox/*.txt"  # Replace with your file pattern
+#pattern = "json*.txt"
+area_threshold = 100  # You can set your own threshold here
+box_areas, removed_box_areas = process_files(pattern, area_threshold)
+
+plt.hist(box_areas, bins=50, range=(0, 1000))
+plt.title("Box Area Histogram")
+plt.xlabel("Area")
+plt.ylabel("Frequency")
+plt.show()
+#plt.hist(removed_box_areas, bins=50)
+#plt.title("Removed Box Area Histogram")
+#plt.xlabel("Area")
+#plt.ylabel("Frequency")
+#plt.show()
