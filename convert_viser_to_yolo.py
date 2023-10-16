dac854485fee72211f0267beadb1d0cb89617760
stjoly <joakim.linden@saabgroup.com>
2023-10-17 01:02:52 +0200

    replaced the split function with partition_dataset, which assumes a number of folder of data. script also creates the dataset yaml file.

--- convert_viser_to_yolo.py
+++ convert_viser_to_yolo.py
@@ -3,6 +3,7 @@ import random
 import argparse
 import json
 from glob import glob
+import yaml
 
 def create_yolo_labels_folder(folder):
     labels_folder = os.path.join(folder, "")
@@ -68,24 +69,104 @@ def split_dataset(out_rgb_folder, train_ratio, test_ratio, valid_ratio):
         for path in valid_paths:
             valid_file.write(f"{os.path.abspath(path)}\n")
 
+
+def partition_dataset(dataset_folder, train_folders, validation_folders, test_folders, exp_name):
+    train_paths = []
+    val_paths = []
+    test_paths = []
+
+    for folder in train_folders:
+        train_paths.append(glob(os.path.join(dataset_folder, folder, "out_rgb", "*.png")))
+    for folder in validation_folders:
+        val_paths.append(glob(os.path.join(dataset_folder, folder, "out_rgb", "*.png")))
+    for folder in test_folders:
+        test_paths.append(glob(os.path.join(dataset_folder, folder, "out_rgb", "*.png")))
+
+    total_trains = len(train_paths)
+    total_valids = len(val_paths)
+    total_tests = len(test_paths)
+
+
+    with open(os.path.join(dataset_folder, f"train_{exp_name}.txt"), "w") as train_file:
+        for folder in train_paths:
+            for path in folder:
+                train_file.write(f"{os.path.abspath(path)}\n")
+
+
+    with open(os.path.join(dataset_folder, f"valid_{exp_name}.txt"), "w") as valid_file:
+        for folder in val_paths:
+            for path in folder:
+                valid_file.write(f"{os.path.abspath(path)}\n")
+
+    with open(os.path.join(dataset_folder, f"test_{exp_name}.txt"), "w") as test_file:
+        for folder in test_paths:
+            for path in folder:
+                test_file.write(f"{os.path.abspath(path)}\n")
+
+
+def get_list_of_data_folders(folder):
+    folders = [f"world_{n}_drones_1" for n in range(8)]
+    return folders
+
+def permute_list(list, shift=0):
+    return list[shift:] + list[:shift]
+
+
+
+def create_dataset_yaml(exp_name, folder):
+
+    data = {
+        'train': f"./{folder}/train_{exp_name}.txt",
+        'val':   f"./{folder}/valid_{exp_name}.txt",
+        'test':  f"./{folder}/test_{exp_name}.txt",
+        'nc': 4,
+        'names': ['multirotor', 'fixedwing', 'airliner', 'bird']
+    }
+    yaml_file_name = f"{os.path.join(folder, exp_name)}_dataset.yaml"
+    with open(yaml_file_name, 'w') as yaml_file:
+        yaml.dump(data, yaml_file, default_flow_style=False)
+
+    command = f"python train_aux.py --img 640 --batch 16 --epochs 10 --data {yaml_file_name} --cfg ./cfg/training/yolov7-w6.yaml --weights '' --name {exp_name}"
+    command_file_name = f"{os.path.join(folder, exp_name)}_train.sh"
+    with open(command_file_name, "a") as script_file:
+        script_file.write(command + "\n")
+
+
+
 def main():
     parser = argparse.ArgumentParser(description="Convert annotation files to YOLO format and split the dataset.")
     parser.add_argument("--folder", required=True, help="Path to the folder containing 'out_rgb' and 'out_bbox'.")
-    parser.add_argument("--train_ratio", type=float, default=0.7, help="Train set ratio (default: 0.7).")
-    parser.add_argument("--test_ratio", type=float, default=0.2, help="Test set ratio (default: 0.2).")
-    parser.add_argument("--valid_ratio", type=float, default=0.1, help="Validation set ratio (default: 0.1).")
+    parser.add_argument("--train_num", type=int, default=5, help="Number of training folders (default: 5).")
+    parser.add_argument("--test_num,", type=int, default=1, help="Number of test folders (default: 1).")
+    parser.add_argument("--valid_num", type=int, default=2, help="Number of validation folders (default: 2).")
+    parser.add_argument("--experiment_name", default="test")
+    parser.add_argument("--shift", type=int, default=0, help="how many steps to rotate data folders before splitting (default: 0)")
+
     args = parser.parse_args()
+    dlist = get_list_of_data_folders([])
+    shifted_list = permute_list(dlist, args.shift)
 
-    out_rgb_folder = os.path.join(args.folder, "out_rgb")
-    labels_folder = create_yolo_labels_folder(out_rgb_folder)
+    for folder in shifted_list:
 
-    out_bbox_folder = os.path.join(args.folder, "out_bbox")
-    annotation_files = glob(os.path.join(out_bbox_folder, "*.txt"))
+        out_rgb_folder = os.path.join(args.folder, folder, "out_rgb")
+        labels_folder = create_yolo_labels_folder(out_rgb_folder)
 
-    for annotation_file in annotation_files:
-        convert_annotation(annotation_file, labels_folder, out_rgb_folder)
+        out_bbox_folder = os.path.join(args.folder, folder, "out_bbox")
+        annotation_files = glob(os.path.join(out_bbox_folder, "*.txt"))
 
+        for annotation_file in annotation_files:
+            convert_annotation(annotation_file, labels_folder, out_rgb_folder)
+
+    #if i < args.train_num:
+    #elif < i (args.train_num+args.val_num):
     #split_dataset(out_rgb_folder, args.train_ratio, args.test_ratio, args.valid_ratio)
+    train_list = shifted_list[0:args.train_num]
+    val_list = shifted_list[args.train_num:args.train_num+args.valid_num]
+    test_list = shifted_list[args.train_num+args.valid_num:]
+    exp_name = f"exp_{args.shift}"
+    partition_dataset(args.folder, train_list, val_list, test_list, exp_name)
+    create_dataset_yaml(exp_name, args.folder)
+
 
 if __name__ == "__main__":
     main()
