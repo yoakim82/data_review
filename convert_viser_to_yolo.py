591e8964831e0e1e7a64152858c79e59e2286013
stjoly <joakim.linden@saabgroup.com>
2023-10-30 13:22:38 +0100

    adding drone-vs-birds handling to script

--- convert_viser_to_yolo.py
+++ convert_viser_to_yolo.py
@@ -1,10 +1,13 @@
 import os
+import pdb
 import random
 import argparse
 import json
 from glob import glob
 import yaml
 import cv2
+from tqdm import tqdm
+
 
 def create_yolo_labels_folder(folder):
     labels_folder = os.path.join(folder, "")
@@ -133,18 +136,66 @@ def create_dataset_yaml(exp_name, folder):
         script_file.write(command + "\n")
 
 
-def convert_drone_vs_bird_to_yolo(data_folder, annotation_folder, filename, output_dir):
+def find_matching_file(folder, pattern, extensions):
+    for ext in extensions:
+        gurka = os.path.join(folder, f"{pattern}.{ext}")
+        files = glob(gurka)
+        if files:
+            return files[0]  # Return the first matching file found
+    return None  # Return None if no matching file is found
+
+
+def list_of_frames(file_path):
+
+    detected_frames = []
+
+    with open(file_path, "r") as text_file:
+        lines = text_file.readlines()
+
+    for i, line in enumerate(lines):
+        values = line.split()
+        has_object = int(values[1])
+        detected_frames.append((i, line))
+
+    return detected_frames
+
+
+def filter_frames(lines_w_detections, num_frames_to_save):
+
+    random.shuffle(lines_w_detections)
+
+    selected_lines = lines_w_detections[0:num_frames_to_save]
+
+    sorted_lines = sorted(selected_lines, key=lambda x: x[0])
+    return sorted_lines
+
+
+def get_frame(cap, frame_number):
+    # Set the video capture object to the desired frame number
+    cap.set(cv2.CAP_PROP_POS_FRAMES, frame_number)
+
+    # Read the frame
+    return cap.read()
+
+
+def convert_drone_vs_bird_to_yolo(data_folder, annotation_folder, filename, output_dir, num_frames_to_save):
     # Define input file paths
-    video_file_path = os.path.join(data_folder, f"{filename}.mp4")
+    print(f"processing file {filename}, extracting {num_frames_to_save} random images.")
+    possible_extensions = ["avi", "mpg", "mp4", "AVI", "MPG", "MP4"]
+    video_file_path = find_matching_file(data_folder, filename, possible_extensions)
     text_file_path = os.path.join(annotation_folder, f"{filename}.txt")
 
+    data = []
     # Create an output directory to store images and annotations
 
     os.makedirs(output_dir, exist_ok=True)
 
     # Open the text file to read detection data
-    with open(text_file_path, "r") as text_file:
-        lines = text_file.readlines()
+    #with open(text_file_path, "r") as text_file:
+    #    lines = text_file.readlines()
+
+    lines_w_detections = list_of_frames(text_file_path)
+    selected_detections = filter_frames(lines_w_detections, num_frames_to_save)
 
     # Open the video file for frame extraction
     cap = cv2.VideoCapture(video_file_path)
@@ -157,52 +208,95 @@ def convert_drone_vs_bird_to_yolo(data_folder, annotation_folder, filename, outp
     frame_width = int(cap.get(3))
     frame_height = int(cap.get(4))
 
-    for line in lines:
+    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    print(f"Total frames in the video: {total_frames}, size of frames: {frame_width}, {frame_height}")
+
+    #ret, frame = get_frame(cap, frame_number)
+
+    for (frame_number, line) in tqdm(selected_detections):
         values = line.split()
         has_object = int(values[1])
 
         # always read the frame from the video (to stay in sync)
-        ret, frame = cap.read()
+        ret, frame = get_frame(cap, frame_number)
         if not ret:
             print(f"reached end of file, frame number = {frame_number}.")
             break
+        #print(f"processing frame {frame_number}")
+        if has_object >= 1:
+            boxes = []
+            classes = []
+            for i in range(has_object):
+                x, y, w, h = map(int, values[2+i*5:6+i*5])
+                class_name = values[6+5*i].strip()
 
-        if has_object == 1:
-            x, y, w, h = map(int, values[2:6])
-            class_name = values[6].strip()
+                box = [[x, y], [x+w, y+h]]
+                boxes.append(box)
+                classes.append(class_name)
 
-            # Read the frame from the video
-
+            yolo_bboxes = convert_to_yolo_format(bboxes=boxes, image_width=frame_width, image_height=frame_height)
 
             # Save the frame as an image
-            frame_filename = f"{filename}_fr{frame_number}.png"
-            frame_path = os.path.join(output_dir, frame_filename)
-            cv2.imwrite(frame_path, frame)
+            fname = f"{filename}_fr{frame_number}"
+            data.append([classes, yolo_bboxes, fname, frame])
 
-            box = [[x, y], [x+w, y+h]]
-            yolo_bboxes = convert_to_yolo_format(bboxes=[box], image_width=frame_width, image_height=frame_height)
+    #random.shuffle(data)
+    #selected_data = data[:num_frames_to_save]
 
-            #label_filename = os.path.join(labels_folder, os.path.basename(annotation_file).replace(".txt", ".txt"))
-            annotation_filename = os.path.join(output_dir, f"{filename}_fr{frame_number}.txt")
-            with open(annotation_filename, "w") as label_file:
-                for i, bbox in enumerate(yolo_bboxes):
-                    # assign the class labels according to this lookup table:
+    # for convenience, unpack data record after shuffling
+    for (classes, yolo_bboxes, filename, frame) in tqdm(data):
 
-                    class_definition = {"drone": 0,  # multirotor
-                                        "5": 1,  # fixedwing
-                                        "6": 2,  # airliner
-                                        "7": 3  # bird
-                                        }
-                    class_label = class_definition[class_name]
+        frame_path = os.path.join(output_dir, f"{filename}.png")
+        cv2.imwrite(frame_path, frame)
 
-                    label_file.write(f"{class_label} {' '.join(map(str, bbox))}\n")
+        annotation_path = os.path.join(output_dir, f"{filename}.txt")
+        with open(annotation_path, "w") as label_file:
+            for i, bbox in enumerate(yolo_bboxes):
+                # assign the class labels according to this lookup table:
 
-        frame_number += 1
+                class_definition = {"drone": 0,  # multirotor
+                                    "fixedwing": 1,  # fixedwing
+                                    "airliner": 2,  # airliner
+                                    "bird": 3  # bird
+                                    }
+                class_label = class_definition[classes[i]]
+
+                label_file.write(f"{class_label} {' '.join(map(str, bbox))}\n")
+
 
     # Release the video capture object and clean up
     cap.release()
     cv2.destroyAllWindows()
 
+
+def setup_dataset(dataset_folder, test_folders, exp_name):
+    test_paths = []
+
+    for folder in test_folders:
+        test_paths.append(glob(os.path.join(dataset_folder, folder, "*.png")))
+
+    total_tests = len(test_paths)
+
+    with open(os.path.join(dataset_folder, f"test_{exp_name}.txt"), "w") as test_file:
+        for folder in test_paths:
+            for path in folder:
+                test_file.write(f"{os.path.abspath(path)}\n")
+
+
+def count_frames_w_annotations(annotation_folder):
+    annotations = []
+    for filename in glob(os.path.join(annotation_folder, "*.txt")):
+        with open(filename, "r") as text_file:
+            lines = text_file.readlines()
+        count = 0
+        for line in lines:
+            values = line.split()
+            has_object = int(values[1])
+            if has_object > 0:
+                count+= 1
+
+        annotations.append(count)
+    return annotations
 
 
 def main():
@@ -213,37 +307,67 @@ def main():
     parser.add_argument("--valid_num", type=int, default=2, help="Number of validation folders (default: 2).")
     parser.add_argument("--experiment_name", default="test")
     parser.add_argument("--shift", type=int, default=0, help="how many steps to rotate data folders before splitting (default: 0)")
+    parser.add_argument("--source", type=str, default="carla", help="use 'carla' if input is synthetic data or 'drones-vs-birds' for real test data (default: 'carla')")
 
     args = parser.parse_args()
-    dlist = get_list_of_data_folders([])
-    shifted_list = permute_list(dlist, args.shift)
 
-    for folder in shifted_list:
+    if args.source == "carla":
+        dlist = get_list_of_data_folders([])
+        shifted_list = permute_list(dlist, args.shift)
 
-        out_rgb_folder = os.path.join(args.folder, folder, "out_rgb")
-        labels_folder = create_yolo_labels_folder(out_rgb_folder)
+        for folder in shifted_list:
 
-        out_bbox_folder = os.path.join(args.folder, folder, "out_bbox")
-        annotation_files = glob(os.path.join(out_bbox_folder, "*.txt"))
+            out_rgb_folder = os.path.join(args.folder, folder, "out_rgb")
+            labels_folder = create_yolo_labels_folder(out_rgb_folder)
 
-        for annotation_file in annotation_files:
-            convert_annotation(annotation_file, labels_folder, out_rgb_folder)
+            out_bbox_folder = os.path.join(args.folder, folder, "out_bbox")
+            annotation_files = glob(os.path.join(out_bbox_folder, "*.txt"))
 
-    #if i < args.train_num:
-    #elif < i (args.train_num+args.val_num):
-    #split_dataset(out_rgb_folder, args.train_ratio, args.test_ratio, args.valid_ratio)
-    train_list = shifted_list[0:args.train_num]
-    val_list = shifted_list[args.train_num:args.train_num+args.valid_num]
-    test_list = shifted_list[args.train_num+args.valid_num:]
-    exp_name = f"exp_{args.shift}"
-    partition_dataset(args.folder, train_list, val_list, test_list, exp_name)
-    create_dataset_yaml(exp_name, args.folder)
+            for annotation_file in annotation_files:
+                convert_annotation(annotation_file, labels_folder, out_rgb_folder)
 
+        #if i < args.train_num:
+        #elif < i (args.train_num+args.val_num):
+        #split_dataset(out_rgb_folder, args.train_ratio, args.test_ratio, args.valid_ratio)
+        train_list = shifted_list[0:args.train_num]
+        val_list = shifted_list[args.train_num:args.train_num+args.valid_num]
+        test_list = shifted_list[args.train_num+args.valid_num:]
+        exp_name = f"exp_{args.shift}"
+        partition_dataset(args.folder, train_list, val_list, test_list, exp_name)
+        create_dataset_yaml(exp_name, args.folder)
+
+    elif args.source == "drone-vs-birds":
+        save_ratio = 0.03
+        exp_name = f"drone_vs_birds_{args.shift}"
+        #partition_dataset(args.folder, train_list, val_list, test_list, exp_name)
+
+        data_folder = os.path.join(args.folder, "train_videos")
+        annotation_folder = os.path.join(args.folder, "annotations")
+        num_drone_frames = count_frames_w_annotations(annotation_folder)
+        num_frames_to_save = [int(x * save_ratio) for x in num_drone_frames]
+        output_dir = os.path.join(args.folder, "yolo_annotations")
+
+        startFromHere = False
+        print("Extracting frames from video sequences and converting to yolo format.")
+        for (num_frames, filename) in zip(num_frames_to_save, glob(os.path.join(annotation_folder, "*.txt"))):
+            fname = os.path.basename(filename).split(".")[0]
+
+            #if os.path.basename(filename) == "2019_10_16_C0003_3633_inspire.txt":
+            #    startFromHere = True
+
+            if startFromHere:
+                convert_drone_vs_bird_to_yolo(data_folder=data_folder,
+                                              filename=fname,
+                                              annotation_folder=annotation_folder,
+                                              output_dir=output_dir,
+                                              num_frames_to_save=num_frames)
+
+            file_paths = glob(os.path.join(output_dir, "*.png"))
+            with open(os.path.join(args.folder, f"{exp_name}.txt"), "w") as test_file:
+                for path in file_paths:
+                    test_file.write(f"{os.path.abspath(path)}\n")
+            #create_dataset_yaml(exp_name, args.folder)
 
 if __name__ == "__main__":
-    #main()
-
-    convert_drone_vs_bird_to_yolo(data_folder="drone-vs-birds",
-                                  annotation_folder=os.path.join("drone-vs-birds", "annotations"),
-                                  filename="00_01_52_to_00_01_58", output_dir=os.path.join("drone-vs-birds", "yolo_annotations"))
+    main()
 
